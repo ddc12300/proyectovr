4ff19e9d657c62ae6eb4e908d8e6eef38e6f6cb3
ddc12300 <116378134+ddc12300@users.noreply.github.com>
2023-01-12 09:02:41 +0100

    maquina boxeo mejorada programacion

--- Assets/Scripts/Maquina boxeo/boxeo.cs
+++ Assets/Scripts/Maquina boxeo/boxeo.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class boxeo : MonoBehaviour
 {
+    public TMP_Text record1;
+    public TMP_Text record2;
+    private IEnumerator coroutine;
+    private int puntuacion = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        record1.text = puntuacion.ToString(); 
 
+        record2.text = "0";
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        record1.text = puntuacion.ToString();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -25,14 +33,22 @@ public class boxeo : MonoBehaviour
             {
                 Vector3 velocity = rb.velocity;
                 float speed = velocity.magnitude;
-                float score = Mathf.Clamp01(speed / 10f) * 999f;
+                float score = Mathf.Clamp01(speed / 5f) * 999f;
                 Debug.Log("Velocity: " + speed + " m/s");
                 for (int i = 0; i <= score; i++)
                 {
+                    puntuacion = i;
                     Debug.Log("Score: " + i + " points");
+                    coroutine = Timer();
+                    StartCoroutine(coroutine);
                 }
                 
             }
         }
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(1);
     }
 }
