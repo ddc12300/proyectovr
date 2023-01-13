fe486e429f03184b8422d2e6b97c11793fe61475
davidmarmolg <david.marmolg@autonoma.cat>
2023-01-13 17:14:43 +0100

    fuente basquet (arreglar script pelota dani)

--- Assets/Scripts/manos/DisableHandCollider.cs
+++ Assets/Scripts/manos/DisableHandCollider.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DisableHandCollider : MonoBehaviour
@@ -6,6 +8,7 @@ public class DisableHandCollider : MonoBehaviour
     public Collider leftHandCollider;
     // Referencia al collider de la mano derecha
     public Collider rightHandCollider;
+    private IEnumerator coroutine;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,5 +20,24 @@ public class DisableHandCollider : MonoBehaviour
             // Desactivar la propiedad "IsTrigger" del collider de la mano derecha
             rightHandCollider.isTrigger = false;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Si el objeto con el que se ha colisionado no tiene el tag "pelota"
+        if (other.CompareTag("mano"))
+        {
+            coroutine = Timer();
+            StartCoroutine(coroutine);
+            // Desactivar la propiedad "IsTrigger" del collider de la mano izquierda
+            leftHandCollider.isTrigger = true;
+            // Desactivar la propiedad "IsTrigger" del collider de la mano derecha
+            rightHandCollider.isTrigger = true;
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(2);
     }
 }
