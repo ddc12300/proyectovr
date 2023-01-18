dd35ea578ebdaaf04be0d79594f463b74df0496c
ddc12300 <116378134+ddc12300@users.noreply.github.com>
2023-01-18 23:13:37 +0100

    stacker + bolas + fisicas + colliders

---
+++ Assets/Scripts/bolas/SpawnObjects.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject container;
+    public Vector3 localPosition;
+    public int count;
+    private int currentCount = 0;
+
+    void Start()
+    {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Spawn();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public void Spawn()
+    {
+        if (currentCount < count)
+        {
+            GameObject newObject = Instantiate(prefab, container.transform.TransformPoint(localPosition), Quaternion.identity);
+            newObject.transform.parent = container.transform;
+            currentCount++;
+        }
+    }
+}
